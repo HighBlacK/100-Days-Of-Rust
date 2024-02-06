867ce06ec11cd13f49ed20a21f06e3dea5246b07
HighBlacK <hcourtilleau@gmail.com>
2024-02-06 22:00:50 +0100

    Day five complete

---
+++ Week-01/src/day_five.rs
@@ -0,0 +1,46 @@
+// Not ideal - It's dumb. It works, but it's not really scallable
+//TODO: Come back and write something better
+fn pair_of_socks(socks: String) -> i32
+{
+    let mut a = 0;
+    let mut b = 0;
+    let mut c = 0;
+    //add more here
+
+    for sock in socks.chars()
+    {
+        match sock
+        {
+            'A' => a += 1,
+            'B' => b += 1,
+            'C' => c += 1,
+            //add more here
+            _ => continue,
+        }
+    }
+
+    if a % 2 != 0
+    {
+        a -= 1;
+    }
+
+    if b % 2 != 0
+    {
+        b -= 1;
+    }
+
+    if c % 2 != 0
+    {
+        c -= 1;
+    }
+
+    return (a + b + c) / 2;
+}
+
+#[test]
+fn test() 
+{
+    assert_eq!(pair_of_socks(String::from("AA")), 1);
+    assert_eq!(pair_of_socks(String::from("ABABC")), 2);
+    assert_eq!(pair_of_socks(String::from("CABBACCC")), 4);
+}
--- Week-01/target/debug/deps/week_01-887048e31b8e0508.d
+++ Week-01/target/debug/deps/week_01-887048e31b8e0508.d
@@ -1,9 +1,10 @@
-C:\Users\hcour\projects\cloned_github\100-Days-Of-Rust\Week-01\target\debug\deps\week_01-887048e31b8e0508.exe: src\main.rs src\day_one.rs src\day_two.rs src\day_three.rs src\day_four.rs
+C:\Users\hcour\projects\cloned_github\100-Days-Of-Rust\Week-01\target\debug\deps\week_01-887048e31b8e0508.exe: src\main.rs src\day_one.rs src\day_two.rs src\day_three.rs src\day_four.rs src\day_five.rs
 
-C:\Users\hcour\projects\cloned_github\100-Days-Of-Rust\Week-01\target\debug\deps\week_01-887048e31b8e0508.d: src\main.rs src\day_one.rs src\day_two.rs src\day_three.rs src\day_four.rs
+C:\Users\hcour\projects\cloned_github\100-Days-Of-Rust\Week-01\target\debug\deps\week_01-887048e31b8e0508.d: src\main.rs src\day_one.rs src\day_two.rs src\day_three.rs src\day_four.rs src\day_five.rs
 
 src\main.rs:
 src\day_one.rs:
 src\day_two.rs:
 src\day_three.rs:
 src\day_four.rs:
+src\day_five.rs:
